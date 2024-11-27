8313bfe4a1c92d28f0ead13c441e0ba18ad471ec
michalk1924 <michalk1924@gmail.com>
2024-11-28 00:46:00 +0200

    add next.config to gitgnore

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-    env: {
-      PUBLIC_DB_CONNECTION: "mongodb+srv://michalk1924:ovRTzbUsFNzdLX2Z@cluster0.4cafa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      JWT_SECRET : "1234456",
-    }
-  };
-  
-  export default nextConfig;
